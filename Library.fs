23f05dad46c91d4dfaaa0eab29cc8a4922092ded
Bjorn Einar Bjartnes <Bjorn.Einar.Bjartnes@4subsea.com>
2021-12-02 01:12:38 +0100

    first

--- Library.fs
+++ Library.fs
@@ -14,7 +14,6 @@ module Input =
        let testdata = readLines "input.txt" 
        Assert.Equal(10,testdata |> Seq.length)
 
-
     [<Fact>]
     let readTestDataAsInt ()= 
        let testdata = readLines "input.txt" 
@@ -23,6 +22,26 @@ module Input =
 
 module Main =
     open Input
-    let _ = readLines ""
-    
+    open Xunit
+    let lines = readLines "input.txt" |> Seq.toList
+
+    let rec makeTuples (list: int list) =
+        match list with
+            | head :: tail :: rest -> (head,tail) :: makeTuples (tail:: rest) 
+            | _ -> [] 
+
+
+    [<Fact>]
+    let checkTestdata()= 
+       let testdata = readLines "input.txt" |> Seq.toList
+       let tuples = makeTuples testdata
+       let count = tuples |> List.where ( fun (a,b) -> b > a) |> List.length
+       Assert.Equal(7, count)
+       
+    [<Fact>]
+    let checkAssignment()= 
+       let testdata = readLines "input1.txt" |> Seq.toList
+       let tuples = makeTuples testdata
+       let count = tuples |> List.where ( fun (a,b) -> b > a) |> List.length
+       Assert.Equal(1581, count)
 

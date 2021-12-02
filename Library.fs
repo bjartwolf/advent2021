9d33f16c2277d911e6585f6b2c0afe1a501b0706
Bjorn Einar Bjartnes <Bjorn.Einar.Bjartnes@4subsea.com>
2021-12-02 01:32:30 +0100

    two

--- Library.fs
+++ Library.fs
@@ -9,39 +9,37 @@ module Input =
             yield System.Int32.Parse(sr.ReadLine ())
     }
 
-    [<Fact>]
-    let readTestData ()= 
-       let testdata = readLines "input.txt" 
-       Assert.Equal(10,testdata |> Seq.length)
-
-    [<Fact>]
-    let readTestDataAsInt ()= 
-       let testdata = readLines "input.txt" 
-       let answer = [ 199; 200; 208; 210;200; 207; 240; 269; 260; 263]
-       Assert.Equal(answer, testdata)
-
 module Main =
     open Input
     open Xunit
     let lines = readLines "input.txt" |> Seq.toList
 
-    let rec makeTuples (list: int list) =
+    let rec makeTriples(list: int list) =
         match list with
-            | head :: tail :: rest -> (head,tail) :: makeTuples (tail:: rest) 
+            | head :: tail :: ttail :: rest -> (head,tail,ttail) :: makeTriples(tail:: ttail:: rest) 
             | _ -> [] 
 
+    let rec makeTuples(list: (int*int*int) list) =
+        match list with
+            | head :: tail :: rest -> (head,tail) :: makeTuples(tail:: rest) 
+            | _ -> [] 
 
     [<Fact>]
     let checkTestdata()= 
        let testdata = readLines "input.txt" |> Seq.toList
-       let tuples = makeTuples testdata
-       let count = tuples |> List.where ( fun (a,b) -> b > a) |> List.length
-       Assert.Equal(7, count)
+       let triples = makeTriples testdata
+       let tuples = makeTuples triples 
+       let count = tuples |> List.where ( fun ((a,b,c), (d,e,f)) ->  (a+b+c) < (d+e+f)) |> List.length
+       Assert.Equal(5, count)
+
+       let sums = [607;618; 618; 617; 647; 716; 769; 792];
+       Assert.Equal<int list>(sums, (triples |> List.map (fun (a,b,c) -> a+b+c)))
        
     [<Fact>]
     let checkAssignment()= 
        let testdata = readLines "input1.txt" |> Seq.toList
-       let tuples = makeTuples testdata
-       let count = tuples |> List.where ( fun (a,b) -> b > a) |> List.length
-       Assert.Equal(1581, count)
+       let triples = makeTriples testdata
+       let tuples = makeTuples triples 
+       let count = tuples |> List.where ( fun ((a,b,c), (d,e,f)) ->  (a+b+c) < (d+e+f)) |> List.length
+       Assert.Equal(1618, count)
 

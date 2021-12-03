8b3eea17bdad28be6795721b915b615bd0bd85e4
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2021-12-03 09:20:06 +0100

    init

---
+++ day3/Library.fs
@@ -0,0 +1,24 @@
+﻿namespace day3
+
+
+module Input =
+    open System.IO
+
+    let readLines (filePath:string)  = seq {
+        use sr = new StreamReader (filePath)
+        while not sr.EndOfStream do
+            let line = sr.ReadLine () 
+            yield line
+    }
+
+module Main =
+    open Input
+    open Xunit
+
+    [<Fact>]
+    let readAllLines ()= 
+      Assert.Equal(12, readLines "input.txt" |> Seq.length)
+      Assert.Equal(1000, readLines "input1.txt" |> Seq.length)
+     
+ 
+ 

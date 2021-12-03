8123d9edffb0cd93e9bcbff32ee781f896c7d8ee
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2021-12-03 17:43:19 +0100

    refactor day 1

--- day1/Library.fs
+++ day1/Library.fs
@@ -2,7 +2,6 @@
 
 module Input =
     open System.IO
-    open Xunit
     let readLines (filePath:string) = seq {
         use sr = new StreamReader (filePath)
         while not sr.EndOfStream do
@@ -15,20 +14,19 @@ module Main =
     let lines = readLines "input.txt" |> Seq.toList
 
     let rec makeTriples(list: int list) =
-        match list with
-            | head :: tail :: ttail :: rest -> (head,tail,ttail) :: makeTriples(tail:: ttail:: rest) 
-            | _ -> [] 
+        list |> List.windowed 3
+             |> List.map (fun (x) -> (x.Head, x.Tail.Head, x.Tail.Tail.Head))
 
     let rec makeTuples(list: (int*int*int) list) =
-        match list with
-            | head :: tail :: rest -> (head,tail) :: makeTuples(tail:: rest) 
-            | _ -> [] 
+        list |> List.windowed 2
+             |> List.map (fun (x) -> (x.Head, x.Tail.Head))
 
     let countIncreasing (inputPath: string): int =
        let input = readLines inputPath |> Seq.toList
        let triples = makeTriples input 
        let tuples = makeTuples triples 
-       tuples |> List.where ( fun ((a,b,c), (d,e,f)) ->  (a+b+c) < (d+e+f)) |> List.length
+       tuples |> List.where ( fun ((a,b,c), (d,e,f)) ->  (a+b+c) < (d+e+f))
+              |> List.length
 
     [<Fact>]
     let checkTestdata()= 

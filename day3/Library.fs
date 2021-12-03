0f6df4773e9dee62fab3ab30e386d225ea478e81
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2021-12-03 17:28:27 +0100

    make it smaller

--- day3/Library.fs
+++ day3/Library.fs
@@ -75,7 +75,7 @@ module Common =
         let bitArray = new BitArray(count)
         for i in [0 .. (count - 1)] do
            bitArray.[count - (i+1)] <- input.Get(i) 
-        let resultBytes = Array.create<byte> 16 0uy
+        let resultBytes = Array.create<byte> 4 0uy
         bitArray.CopyTo(resultBytes, 0);
         BitConverter.ToInt32(resultBytes,0)
 

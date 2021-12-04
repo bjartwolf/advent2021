a0ff611a88906ba776b023bc7308409fca063b03
Bjorn Einar Bjartnes <Bjorn.Einar.Bjartnes@4subsea.com>
2021-12-04 15:41:11 +0100

    add day 5 to be ready tomorrow

---
+++ day5/Library.fs
@@ -0,0 +1,19 @@
+﻿namespace day5
+
+module Input =
+    open System.IO
+    open Xunit
+    open System
+
+    let readLines (filePath:string): string seq  = seq {
+        use sr = new StreamReader (filePath)
+        while not sr.EndOfStream do
+            yield sr.ReadLine()
+        }
+
+    [<Fact>]
+    let ReadlDataPart1() =
+        let x  = readLines "input.txt"
+        Assert.Equal(1, x |> Seq.length)
+
+

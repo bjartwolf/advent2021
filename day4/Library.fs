d639abe9038e637b38d0f10f59e35e95229c9933
Bjorn Einar Bjartnes <Bjorn.Einar.Bjartnes@4subsea.com>
2021-12-04 10:12:05 +0100

    board works

--- day4/Library.fs
+++ day4/Library.fs
@@ -22,10 +22,20 @@ module Input =
                                 | Marked -> true
                                 | Nr _ -> false)
 
+    let isWinnerBoard (board: Board) : bool =
+        board |> Array.exists ( fun r -> isWinnerRow r)
+
     [<Fact>]
     let RowdWithMarkedRowIsWinner ()= 
         let r1 = [| Marked; Marked ; Marked ; Marked; Marked|]
         Assert.Equal(true, isWinnerRow r1)        
+
+    [<Fact>]
+    let BlarddWithMarkedRowIsWinner ()= 
+        let r1 = [| Marked; Nr 4 ; Marked ; Marked; Marked|]
+        let r2 = [| Marked; Marked ; Marked ; Marked; Marked|]
+        let b = [|r1;r2|]
+        Assert.Equal(true, isWinnerBoard b)        
 
     [<Fact>]
     let RowdWithUndrawnNrIsNotWinner ()= 

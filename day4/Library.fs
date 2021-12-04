c8098627b75e3b8228c7a803fa48461fbf3aec05
Bjorn Einar Bjartnes <Bjorn.Einar.Bjartnes@4subsea.com>
2021-12-04 10:10:03 +0100

    init

--- day4/Library.fs
+++ day4/Library.fs
@@ -11,6 +11,28 @@ module Input =
             yield sr.ReadLine()
         }
 
+    type NumbersToDraw = int list
+    type Field = Marked | Nr of int
+    type Row = Field []
+    type Board = Row [] 
+
+//    let parseLines (filePath: string): 
+    let isWinnerRow (row: Row) : bool =
+        row |> Array.forall ( fun f -> match f with
+                                | Marked -> true
+                                | Nr _ -> false)
+
+    [<Fact>]
+    let RowdWithMarkedRowIsWinner ()= 
+        let r1 = [| Marked; Marked ; Marked ; Marked; Marked|]
+        Assert.Equal(true, isWinnerRow r1)        
+
+    [<Fact>]
+    let RowdWithUndrawnNrIsNotWinner ()= 
+        let r1 = [| Marked; Nr 4 ; Marked ; Marked; Marked|]
+        Assert.Equal(false, isWinnerRow r1)        
+
+
     [<Fact>]
     let readAllLines ()= 
       Assert.Equal(19, readLines "input.txt" |> Seq.length)

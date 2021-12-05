2094987fd22e8d5c627bf85b677d0258d75b958d
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2021-12-05 23:21:42 +0100

    if i want to do this

---
+++ day6/Library.fs
@@ -0,0 +1,19 @@
+﻿namespace day6
+
+module Input =
+    open System.IO
+    open Xunit
+    open System
+
+
+    let readLines (filePath:string): string seq  = seq {
+        use sr = new StreamReader (filePath)
+        while not sr.EndOfStream do
+            let line = sr.ReadLine()
+            yield line 
+        }
+
+    [<Fact>]
+    let ReadlDataPart1() =
+        let x  = readLines "input.txt"
+        Assert.Equal(10, x |> Seq.length)

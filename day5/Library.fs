7f315c18172f8c28c5dbdae94f42333bf02b0db9
Bjorn Einar Bjartnes <Bjorn.Einar.Bjartnes@4subsea.com>
2021-12-05 10:50:26 +0100

    init pattern

--- day5/Library.fs
+++ day5/Library.fs
@@ -6,8 +6,12 @@ module Input =
     open System
 
     type Point = { X: int; Y: int } 
-    type LineSegment = { S: Point; E: Point }
-    type LineType = VerticalLine of LineSegment | HorizontalLine of LineSegment 
+    type LS = { S: Point; E: Point }
+    type LineType = Vertical of LS | Horizontal of LS 
+
+    let getLineTypeFromLineSegment (ls: LS ): LineType =
+        match ls with
+            | { S = { X = x1; Y = y1}; E = { X = x2; Y = y2}} -> Vertical ls
 
     let readLines (filePath:string): LineType option seq  = seq {
         use sr = new StreamReader (filePath)
@@ -16,7 +20,7 @@ module Input =
             let p1 = { X = 0; Y = 0} 
             let p2 = { X = 10; Y = 0} 
             let s = {S = p1; E = p2}
-            yield Some (VerticalLine s)
+            yield Some (Vertical s)
         }
 
     [<Fact>]

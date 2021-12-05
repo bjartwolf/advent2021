b4f6e1cf896e80b86a98d012145691eb239bfab7
Bjorn Einar Bjartnes <Bjorn.Einar.Bjartnes@4subsea.com>
2021-12-05 10:58:51 +0100

    finding their types

--- day5/Library.fs
+++ day5/Library.fs
@@ -9,18 +9,28 @@ module Input =
     type LS = { S: Point; E: Point }
     type LineType = Vertical of LS | Horizontal of LS 
 
-    let getLineTypeFromLineSegment (ls: LS ): LineType =
+    let getLineTypeFromLineSegment (ls: LS ): LineType option =
         match ls with
-            | { S = { X = x1; Y = y1}; E = { X = x2; Y = y2}} -> Vertical ls
+            | { S = { X = x1; Y = y1};
+                E = { X = x2; Y = y2}} when x1 = x2 && y1 <> y2 -> Some (Vertical ls)
+            | { S = { X = x1; Y = y1};
+                E = { X = x2; Y = y2}} when x1 <> x2 && y1 = y2 -> Some (Horizontal ls)
+            | _ -> None
 
     let readLines (filePath:string): LineType option seq  = seq {
         use sr = new StreamReader (filePath)
         while not sr.EndOfStream do
             let line = sr.ReadLine()
-            let p1 = { X = 0; Y = 0} 
-            let p2 = { X = 10; Y = 0} 
+            let lineparts = line.Split (" -> ")
+            let p1raw = lineparts.[0]
+            let p2raw = lineparts.[1]
+            let x1y1raw = p1raw.Split(",")
+            let x2y2raw = p2raw.Split(",")
+            let p1 = { X = Int32.Parse(x1y1raw.[0]); Y = Int32.Parse(x1y1raw.[1])} 
+            let p2 = { X = Int32.Parse(x2y2raw.[0]); Y = Int32.Parse(x2y2raw.[1])} 
             let s = {S = p1; E = p2}
-            yield Some (Vertical s)
+            let lt = getLineTypeFromLineSegment s
+            yield lt 
         }
 
     [<Fact>]

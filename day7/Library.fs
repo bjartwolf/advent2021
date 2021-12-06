0b13c5ecd25d56fd15ef055a6039e383c4dda005
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2021-12-06 22:04:54 +0100

    init but I did give up

---
+++ day7/Library.fs
@@ -0,0 +1,18 @@
+﻿namespace day7
+
+module Input =
+    open System
+    open System.IO
+    open Xunit 
+
+    let readInit (filePath: string): int list = 
+        use sr = new StreamReader (filePath) 
+        let line = sr.ReadLine()
+        let numbers = line.Split(",")
+        numbers |> Array.map(fun f -> Int32.Parse(f)) |> Array.toList
+
+    [<Fact>]
+    let test2 () = 
+        let input = readInit "input.txt" 
+        Assert.Equal(5, input.Length) 
+ 

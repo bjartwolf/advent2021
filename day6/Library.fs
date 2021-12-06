e23a2419576901bfe6cad6adc04f955c8eae10e4
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2021-12-06 06:15:25 +0100

    read input as fish

--- day6/Library.fs
+++ day6/Library.fs
@@ -5,15 +5,26 @@ module Input =
     open Xunit
     open System
 
-    //let firstCycle = 2
-    let firstCycle = 1
-    
-    type Newborn = FirstCycle of int
+    type Newborn = FirstCycle of int list
+    type LanternFish = DaysLeft of int list
+    type Fish = Baby of Newborn | Adult of LanternFish 
 
-    //let nrOfDay = 7
-    let nrOfDay = 6
-    let counter = [0 .. nrOfDay]
-    type LanternFish = DaysLeft of int
+    let createLanternFishFromDays (days: int) : LanternFish =
+        let counter = [0 .. days] // starts a bit off
+        DaysLeft counter
+ 
+    let createLanternFish () : LanternFish =
+        let nrOfDay = 7
+        let counter = [0 .. nrOfDay - 1]
+        DaysLeft counter
+    
+    let createNewbornFish () : Newborn =
+        let nrOfDay = 2
+        let counter = [0 .. nrOfDay - 1]
+        FirstCycle counter
+
+
+
 
     let readInit (filePath: string): int list = 
         use sr = new StreamReader (filePath) 
@@ -21,7 +32,13 @@ module Input =
         let numbers = line.Split(",")
         numbers |> Array.map(fun f -> Int32.Parse(f)) |> Array.toList
 
+    let getInitialFish (filePath: string): LanternFish list =
+        let numbers = readInit filePath
+        numbers |> List.map (fun f -> createLanternFishFromDays f)
+
     [<Fact>]
     let ReadlDataPart1() =
         let x  = readInit "input.txt"
+        let fish  = getInitialFish "input.txt" 
         Assert.Equal(5, x |> List.length)
+        Assert.Equal(5, fish |> List.length)

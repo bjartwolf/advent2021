5139907c97b8c79395d73e9c0a8e2feed6832183
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2021-12-06 06:31:33 +0100

    read input as fish

--- day6/Library.fs
+++ day6/Library.fs
@@ -5,26 +5,25 @@ module Input =
     open Xunit
     open System
 
-    type Newborn = FirstCycle of int list
-    type LanternFish = DaysLeft of int list
+    type Newborn = int
+    type LanternFish = int
     type Fish = Baby of Newborn | Adult of LanternFish 
 
-    let createLanternFishFromDays (days: int) : LanternFish =
-        let counter = [0 .. days] // starts a bit off
-        DaysLeft counter
+    let createLanternFishFromDays (days: int) : Fish =
+        Adult days 
  
     let createLanternFish () : LanternFish =
         let nrOfDay = 7
-        let counter = [0 .. nrOfDay - 1]
-        DaysLeft counter
+        nrOfDay - 1 
     
     let createNewbornFish () : Newborn =
         let nrOfDay = 2
-        let counter = [0 .. nrOfDay - 1]
-        FirstCycle counter
+        nrOfDay - 1 
 
-
-
+    let passDay (fish: Fish) : (Fish * Newborn option) =
+        match fish with 
+            | Baby nb -> if nb = 0 then (Adult (createLanternFish ()), None) else (Baby (nb - 1), None)
+            | Adult a -> if a = 0 then (Adult (createLanternFish ()), Some (createNewbornFish ())) else (Adult (a - 1), None)
 
     let readInit (filePath: string): int list = 
         use sr = new StreamReader (filePath) 
@@ -32,12 +31,19 @@ module Input =
         let numbers = line.Split(",")
         numbers |> Array.map(fun f -> Int32.Parse(f)) |> Array.toList
 
-    let getInitialFish (filePath: string): LanternFish list =
+    let getInitialFish (filePath: string): Fish list =
         let numbers = readInit filePath
         numbers |> List.map (fun f -> createLanternFishFromDays f)
 
     [<Fact>]
-    let ReadlDataPart1() =
+    let ReadlDataPart1CheckFish() =
+        let fish  = getInitialFish "input.txt" 
+        Assert.Equal(Adult 3, fish.Head)
+        let fishes = fish |> List.map (fun f -> passDay f)
+        Assert.Equal(fishes.Head, (Adult 2, None))
+
+    [<Fact>]
+    let ReadlDataPart1 () =
         let x  = readInit "input.txt"
         let fish  = getInitialFish "input.txt" 
         Assert.Equal(5, x |> List.length)
